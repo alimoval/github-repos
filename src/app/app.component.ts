8c0c8f84525d237fef0d529ae0e1fcc6a3799357
Alimov Alik <aalimov@ad.geeksforless.net>
2017-02-28 16:23:01 +0200

    empty input error fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Observable } from "rxjs/Observable";
 //import "rxjs/add/observable/fromPromise";
 import "rxjs/add/operator/debounceTime";
 import "rxjs/add/operator/map";
-//import "rxjs/add/operator/do";
+import "rxjs/add/operator/filter";
 import "rxjs/add/operator/switchMap";
 
 @Component({
@@ -22,8 +22,8 @@ export class AppComponent implements OnInit {
   public form: FormGroup;
   public inputEvents: Subject<KeyboardEvent> = new Subject<KeyboardEvent>();
 
-  onInput($event: KeyboardEvent) {
-    this.inputEvents.next($event);
+  onInput(query: KeyboardEvent) {
+    this.inputEvents.next(query);
   }
 
   constructor(private _fb: FormBuilder, private _http: Http) {
@@ -35,10 +35,11 @@ export class AppComponent implements OnInit {
     this.inputEvents
       .debounceTime(800)
       .map((event: KeyboardEvent) => (event.target as HTMLInputElement).value)
-      .switchMap((query: string) =>  
-        this._http.get('https://api.github.com/search/repositories?q='+query)
+      .filter((query: string) => query.length>=1)
+      .switchMap((query: string) =>
+        this._http.get('https://api.github.com/search/repositories?q=' + query)
       )
       .map(res => res.json())
-      .subscribe(res => {this.repos = res.items});
+      .subscribe(res => { this.repos = res.items });
   }
 }
